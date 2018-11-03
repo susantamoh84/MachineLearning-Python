1a1d2b196ace25f1db5807b0a6a2d3a58eeef154
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-03 19:56:03 +0530

    Update hyperparam-tuning.py

--- XGBoost/hyperparam-tuning.py
+++ XGBoost/hyperparam-tuning.py
@@ -108,3 +108,49 @@ for curr_val in colsample_bytree_vals:
 
 # Print the resultant DataFrame
 print(pd.DataFrame(list(zip(colsample_bytree_vals, best_rmse)), columns=["colsample_bytree","best_rmse"]))
+
+# Create your housing DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter grid: gbm_param_grid
+gbm_param_grid = {
+    'colsample_bytree': [0.3, 0.7],
+    'n_estimators': [50],
+    'max_depth': [2, 5]
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor()
+
+# Perform grid search: grid_mse
+grid_mse = GridSearchCV(estimator=gbm, param_grid=gbm_param_grid, scoring="neg_mean_squared_error",
+cv=4, verbose=1)
+
+
+# Fit grid_mse to the data
+grid_mse.fit(X, y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", grid_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_)))
+
+# Create the parameter grid: gbm_param_grid 
+gbm_param_grid = {
+    'n_estimators': [25],
+    'max_depth': range(2, 12)
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor(n_estimators=10)
+
+# Perform random search: grid_mse
+randomized_mse = RandomizedSearchCV(estimator=gbm, param_distributions=gbm_param_grid, scoring="neg_mean_squared_error", n_iter=5, cv=4, verbose=1)
+
+
+# Fit randomized_mse to the data
+randomized_mse.fit(X, y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", randomized_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(randomized_mse.best_score_)))
+
