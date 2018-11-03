5f6f84c93168dfb0dde9d216a4ee691b8837a13b
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-03 19:34:45 +0530

    Create hyperparam-tuning.py

---
+++ XGBoost/hyperparam-tuning.py
@@ -0,0 +1,110 @@
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(X, y)
+
+# Create the parameter dictionary for each tree: params 
+params = {"objective":"reg:linear", "max_depth":3}
+
+# Create list of number of boosting rounds
+num_rounds = [5, 10, 15]
+
+# Empty list to store final round rmse per XGBoost model
+final_rmse_per_round = []
+
+# Iterate over num_rounds and build one model per num_boost_round parameter
+for curr_num_rounds in num_rounds:
+
+    # Perform cross-validation: cv_results
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, num_boost_round=curr_num_rounds, metrics="rmse", as_pandas=True, seed=123)
+    
+    # Append final round RMSE
+    final_rmse_per_round.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+num_rounds_rmses = list(zip(num_rounds, final_rmse_per_round))
+print(pd.DataFrame(num_rounds_rmses,columns=["num_boosting_rounds","rmse"]))
+
+# Create your housing DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary for each tree: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation with early stopping: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, metrics="rmse", num_boost_round=50, seed=123, early_stopping_rounds=10)
+
+# Print cv_results
+print(cv_results)
+
+# Create your housing DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary for each tree (boosting round)
+params = {"objective":"reg:linear", "max_depth":3}
+
+# Create list of eta values and empty list to store final round rmse per xgboost model
+eta_vals = [0.001, 0.01, 0.1]
+best_rmse = []
+
+# Systematically vary the eta 
+for curr_val in eta_vals:
+
+    params["eta"] = curr_val
+    
+    # Perform cross-validation: cv_results
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, num_boost_round=10, early_stopping_rounds=5, metrics='rmse', seed=123)
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(eta_vals, best_rmse)), columns=["eta","best_rmse"]))
+
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X,label=y)
+
+# Create the parameter dictionary
+params = {"objective":"reg:linear"}
+
+# Create list of max_depth values
+max_depths = [2,5,10,20]
+best_rmse = []
+
+# Systematically vary the max_depth
+for curr_val in max_depths:
+
+    params["max_depth"] = curr_val
+    
+    # Perform cross-validation
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=10, early_stopping_rounds=5, metrics="rmse", seed=123)
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(max_depths, best_rmse)),columns=["max_depth","best_rmse"]))
+
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X,label=y)
+
+# Create the parameter dictionary
+params={"objective":"reg:linear","max_depth":3}
+
+# Create list of hyperparameter values: colsample_bytree_vals
+colsample_bytree_vals = [0.1, 0.5, 0.8, 1]
+best_rmse = []
+
+# Systematically vary the hyperparameter value 
+for curr_val in colsample_bytree_vals:
+
+    params["colsample_bytree"] = curr_val
+    
+    # Perform cross-validation
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2,
+                 num_boost_round=10, early_stopping_rounds=5,
+                 metrics="rmse", as_pandas=True, seed=123)
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(colsample_bytree_vals, best_rmse)), columns=["colsample_bytree","best_rmse"]))
