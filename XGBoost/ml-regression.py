b264a76f21336cfe5e887bf10036e927ff9666ed
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-03 19:34:10 +0530

    Update ml-regression.py

--- XGBoost/ml-regression.py
+++ XGBoost/ml-regression.py
@@ -31,3 +31,79 @@ preds = xg_reg.predict(DM_test)
 rmse = np.sqrt(mean_squared_error(y_test,preds))
 print("RMSE: %f" % (rmse))
 
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics='mae', as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-mae-mean"]).tail(1))
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+reg_params = [1, 10, 100]
+
+# Create the initial parameter dictionary for varying l2 strength: params
+params = {"objective":"reg:linear","max_depth":3}
+
+# Create an empty list for storing rmses as a function of l2 complexity
+rmses_l2 = []
+
+# Iterate over reg_params
+for reg in reg_params:
+
+    # Update l2 strength
+    params["lambda"] = reg
+    
+    # Pass this updated param dictionary into cv
+    cv_results_rmse = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+    
+    # Append best rmse (final round) to rmses_l2
+    rmses_l2.append(cv_results_rmse["test-rmse-mean"].tail(1).values[0])
+
+# Look at best rmse per l2 param
+print("Best rmse as a function of l2:")
+print(pd.DataFrame(list(zip(reg_params, rmses_l2)), columns=["l2", "rmse"]))
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":2}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)
+
+# Plot the first tree
+xgb.plot_tree(xg_reg,num_trees=0)
+plt.show()
+
+# Plot the fifth tree
+xgb.plot_tree(xg_reg,num_trees=4)
+plt.show()
+
+# Plot the last tree sideways
+xgb.plot_tree(xg_reg,num_trees=9,rankdir="LR")
+plt.show()
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(X, y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(dtrain=housing_dmatrix, params=params, num_boost_round=10)
+
+# Plot the feature importances
+xgb.plot_importance(xg_reg)
+plt.show()
+
