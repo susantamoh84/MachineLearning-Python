2c707203ab55d4abb044ee77e6bc9cc42c2339d1
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-11 17:43:58 +0530

    Update named-entity-extraction.py

--- NLP/named-entity-extraction.py
+++ NLP/named-entity-extraction.py
@@ -16,3 +16,24 @@ for sent in chunked_sentences:
         if hasattr(chunk, "label") and chunk.label() == "NE":
             print(chunk)
 
+# Create the defaultdict: ner_categories
+ner_categories = defaultdict(int)
+
+# Create the nested for loop
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, 'label'):
+            ner_categories[chunk.label()] += 1
+            
+# Create a list from the dictionary keys for the chart labels: labels
+labels = list(ner_categories.keys())
+
+# Create a list of the values: values
+values = [ner_categories.get(l) for l in labels]
+
+# Create the pie chart
+plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+
+# Display the chart
+plt.show()
+
