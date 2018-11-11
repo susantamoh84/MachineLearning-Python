14502189f9fd2c09470373a616277ee26ba6d779
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-12 00:26:08 +0530

    Update chatbot-101.py

--- ChatBot/chatbot-101.py
+++ ChatBot/chatbot-101.py
@@ -20,3 +20,142 @@ def send_message(message):
 # Send a message to the bot
 send_message("hello")
 
+# Define variables
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
+
+send_message("what's today's weather?")
+send_message("what's your name?")
+send_message("what's your favorite color?")
+
+# Import the random module
+import random
+
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return a random matching response
+        bot_message = random.choice(responses[message])
+    else:
+        # Return a random "default" response
+        bot_message = random.choice(responses["default"])
+    return bot_message
+
+send_message("what's your name?")
+send_message("what's your name?")
+send_message("what's your name?")
+
+import random
+
+def respond(message):
+    # Check for a question mark
+    if message.endswith("?"):
+        # Return a random question
+        return random.choice(responses["question"])
+    # Return a random statement
+    return random.choice(responses["statement"])
+
+
+# Send messages ending in a question mark
+send_message("what's today's weather?")
+send_message("what's today's weather?")
+
+# Send messages which don't end with a question mark
+send_message("I love building chatbots")
+send_message("I love building chatbots")
+
+# Define match_rule()
+def match_rule(rules, message):
+    response, phrase = "default", None
+    
+    # Iterate over the rules dictionary
+    for pattern, responses in rules.items():
+        # Create a match object
+        match = re.search(pattern, message)
+        if match is not None:
+            # Choose a random response
+            response = random.choice(responses)
+            if '{0}' in response:
+                phrase = match.group(1)
+    # Return the response and phrase
+    return response, phrase
+
+# Test match_rule
+print(match_rule(rules, "do you remember your last birthday"))
+
+# Define replace_pronouns()
+def replace_pronouns(message):
+
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub('me', 'you', message)
+    if 'my' in message:
+        # Replace 'my' with 'your'
+        return re.sub('my', 'your',message)
+    if 'your' in message:
+        # Replace 'your' with 'my'
+        return re.sub('your', 'my', message)
+    if 'you' in message:
+        # Replace 'you' with 'me'
+        return re.sub('you', 'me', message)
+
+    return message
+
+print(replace_pronouns("my last birthday"))
+print(replace_pronouns("when you went to Florida"))
+print(replace_pronouns("I had my own castle"))
+
+# Define respond()
+def respond(message):
+    # Call match_rule
+    response, phrase = match_rule(rules, message)
+    if '{0}' in response:
+        # Replace the pronouns in the phrase
+        phrase = replace_pronouns(phrase)
+        # Include the phrase in the response
+        response = response.format(phrase)
+    return response
+
+# Send the messages
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
+
