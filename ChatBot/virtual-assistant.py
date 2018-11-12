a66c581e499b3325d8f6a7d8bf2e6809426f56fc
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-12 12:57:52 +0530

    Update virtual-assistant.py

--- ChatBot/virtual-assistant.py
+++ ChatBot/virtual-assistant.py
@@ -63,3 +63,84 @@ def respond(message):
   
 print(respond("I want an expensive hotel in the south of town"))
 
+# Define a respond function, taking the message and existing params as input
+def respond(message, params):
+    # Extract the entities
+    entities = interpreter.parse(message)["entities"]
+    # Fill the dictionary with entities
+    for ent in entities:
+        params[ent["entity"]] = str(ent["value"])
+
+    # Find the hotels
+    results = find_hotels(params)
+    names = [r[0] for r in results]
+    n = min(len(results), 3)
+    # Return the appropriate response
+    return responses[n].format(*names), params
+
+# Initialize params dictionary
+params = {}
+
+# Pass the messages to the bot
+for message in ["I want an expensive hotel", "in the north of town"]:
+    print("USER: {}".format(message))
+    response, params = respond(message, params)
+    print("BOT: {}".format(response))
+    
+# Define negated_ents()
+def negated_ents(phrase):
+    # Extract the entities using keyword matching
+    ents = [e for e in ["south", "north"] if e in phrase]
+    # Find the index of the final character of each entity
+    ends = sorted([phrase.index(e) + len(e) for e in ents])
+    # Initialise a list to store sentence chunks
+    chunks = []
+    # Take slices of the sentence up to and including each entitiy
+    start = 0
+    for end in ends:
+        chunks.append(phrase[start:end])
+        start = end
+    result = {}
+    # Iterate over the chunks and look for entities
+    for chunk in chunks:
+        for ent in ents:
+            if ent in chunk:
+                # If the entity is preceeded by a negation, give it the key False
+                if "not" in chunk or "n't" in chunk:
+                    result[ent] = False
+                else:
+                    result[ent] = True
+    return result  
+
+# Check that the entities are correctly assigned as True or False
+for test in tests:
+    print(negated_ents(test[0]) == test[1])
+
+# Define the respond function
+def respond(message, params, neg_params):
+    # Extract the entities
+    entities = interpreter.parse(message)["entities"]
+    ent_vals = [e["value"] for e in entities]
+    # Look for negated entities
+    negated = negated_ents(message, ent_vals)
+    for ent in entities:
+        if ent["value"] in negated and negated[ent["value"]]:
+            neg_params[ent["entity"]] = str(ent["value"])
+        else:
+            params[ent["entity"]] = str(ent["value"])
+    # Find the hotels
+    results = find_hotels(params, neg_params)
+    names = [r[0] for r in results]
+    n = min(len(results),3)
+    # Return the correct response
+    return responses[n].format(*names), params, neg_params
+
+# Initialize params and neg_params
+params = {}
+neg_params = {}
+
+# Pass the messages to the bot
+for message in ["I want a cheap hotel", "but not in the north of town"]:
+    print("USER: {}".format(message))
+    response, params, neg_params = respond(message, params, neg_params)
+    print("BOT: {}".format(response))
