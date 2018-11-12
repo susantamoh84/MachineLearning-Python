1e4baa22a3ebf04ee5e4898983d1412ef9360669
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-12 08:19:21 +0530

    Update nlu.py

--- ChatBot/nlu.py
+++ ChatBot/nlu.py
@@ -63,3 +63,22 @@ send_message("my name is David Copperfield")
 send_message("call me Ishmael")
 send_message("People call me Cassandra")
 
+# Load the spacy model: nlp
+nlp = spacy.load('en')
+
+# Calculate the length of sentences
+n_sentences = len(sentences)
+
+# Calculate the dimensionality of nlp
+embedding_dim = nlp.vocab.vectors_length
+
+# Initialize the array with zeros: X
+X = np.zeros((n_sentences, embedding_dim))
+
+# Iterate over the sentences
+for idx, sentence in enumerate(sentences):
+    # Pass each each sentence to the nlp object to create a document
+    doc = nlp(sentence)
+    # Save the document's .vector attribute to the corresponding row in X
+    X[idx, :] = doc.vector
+
