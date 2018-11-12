a049e3420eca9a40fdebdd6ca2e25d3f82fc6df7
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-13 00:26:57 +0530

    Update dialogue.py

--- ChatBot/dialogue.py
+++ ChatBot/dialogue.py
@@ -55,3 +55,146 @@ send_messages([
     "kenyan"
 ])
 
+# Define respond()
+def respond(message, params, prev_suggestions, excluded):
+    # Interpret the message
+    parse_data = interpret(message)
+    # Extract the intent
+    intent = parse_data["intent"]["name"]
+    # Extract the entities
+    entities = parse_data["entities"]
+    # Add the suggestion to the excluded list if intent is "deny"
+    if intent == "deny":
+        excluded.extend(prev_suggestions)
+    # Fill the dictionary with entities
+    for ent in entities:
+        params[ent["entity"]] = str(ent["value"])
+    # Find matching hotels
+    results = [
+        r 
+        for r in find_hotels(params, excluded) 
+        if r[0] not in excluded
+    ]
+    # Extract the suggestions
+    names = [r[0] for r in results]
+    n = min(len(results), 3)
+    suggestions = names[:2]
+    return responses[n].format(*names), params, suggestions, excluded
+
+# Initialize the empty dictionary and lists
+params, suggestions, excluded = {}, [], []
+
+# Send the messages
+for message in ["I want a mid range hotel", "no that doesn't work for me"]:
+    print("USER: {}".format(message))
+    response, params, suggestions, excluded = respond(message, params, suggestions, excluded)
+    print("BOT: {}".format(response))
+
+# Define policy()
+def policy(intent):
+    # Return "do_pending" if the intent is "affirm"
+    if intent == "affirm":
+        return "do_pending", None
+    # Return "Ok" if the intent is "deny"
+    if intent == "deny":
+        return "Ok", None
+    if intent == "order":
+        return "Unfortunately, the Kenyan coffee is currently out of stock, would you like to order the Brazilian beans?", "Alright, I've ordered that for you!"
+
+# Define send_message()
+def send_message(pending, message):
+    print("USER : {}".format(message))
+    action, pending_action = policy(interpret(message))
+    if action == "do_pending" and pending is not None:
+        print("BOT : {}".format(pending))
+    else:
+        print("BOT : {}".format(action))
+    return pending_action
+    
+# Define send_messages()
+def send_messages(messages):
+    pending = None
+    for msg in messages:
+        pending = send_message(pending, msg)
+
+# Send the messages
+send_messages([
+    "I'd like to order some coffee",
+    "ok yes please"
+])
+
+# Define the states
+INIT=0
+AUTHED=1
+CHOOSE_COFFEE=2
+ORDERED=3
+
+# Define the policy rules
+policy_rules = {
+    (INIT, "order"): (INIT, "you'll have to log in first, what's your phone number?", AUTHED),
+    (INIT, "number"): (AUTHED, "perfect, welcome back!", None),
+    (AUTHED, "order"): (CHOOSE_COFFEE, "would you like Columbian or Kenyan?", None),    
+    (CHOOSE_COFFEE, "specify_coffee"): (ORDERED, "perfect, the beans are on their way!", None)
+}
+
+# Define send_messages()
+def send_messages(messages):
+    state = INIT
+    pending = None
+    for msg in messages:
+        state, pending = send_message(state, pending, msg)
+
+# Send the messages
+send_messages([
+    "I'd like to order some coffee",
+    "555-12345",
+    "kenyan"
+])
+
+# Define chitchat_response()
+def chitchat_response(message):
+    # Call match_rule()
+    response, phrase = match_rule(eliza_rules, message)
+    # Return none is response is "default"
+    if response == "default":
+        return None
+    if '{0}' in response:
+        # Replace the pronouns of phrase
+        phrase = replace_pronouns(phrase)
+        # Calculate the response
+        response = response.format(phrase)
+    return response
+
+# Define send_message()
+def send_message(state, pending, message):
+    print("USER : {}".format(message))
+    response = chitchat_response(message)
+    if response is not None:
+        print("BOT : {}".format(response))
+        return state, None
+    
+    # Calculate the new_state, response, and pending_state
+    new_state, response, pending_state = policy_rules[(state, interpret(message))]
+    print("BOT : {}".format(response))
+    if pending is not None:
+        new_state, response, pending_state = policy_rules[pending]
+        print("BOT : {}".format(response))        
+    if pending_state is not None:
+        pending = (pending_state, interpret(message))
+    return new_state, pending
+
+# Define send_messages()
+def send_messages(messages):
+    state = INIT
+    pending = None
+    for msg in messages:
+        state, pending = send_message(state, pending, msg)
+
+# Send the messages
+send_messages([
+    "I'd like to order some coffee",
+    "555-12345",
+    "do you remember when I ordered 1000 kilos by accident?",
+    "kenyan"
+])  
+
