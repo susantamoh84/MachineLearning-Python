f0b24a4d7bafe85d124f86e38a298d7379419b9f
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-12 08:01:15 +0530

    Update nlu.py

--- ChatBot/nlu.py
+++ ChatBot/nlu.py
@@ -1,1 +1,65 @@
 
+# Define a dictionary of patterns
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = '|'.join(keys)
+    
+# Print the patterns
+print(patterns)
+
+# Define a function to find the intent of a message
+def match_intent(message):
+    matched_intent = None
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message 
+        if pattern.search(message):
+            matched_intent = intent
+    return matched_intent
+
+# Define a respond function
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    return responses[key]
+
+# Send messages
+send_message("hello!")
+send_message("bye byeee")
+send_message("thanks very much!")
+
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile("name|call")
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile("[A-Z]{1}[a-z]*")
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+# Define respond()
+def respond(message):
+    # Find the name
+    name = find_name(message)
+    if name is None:
+        return "Hi there!"
+    else:
+        return "Hello, {0}!".format(name)
+
+# Send messages
+send_message("my name is David Copperfield")
+send_message("call me Ishmael")
+send_message("People call me Cassandra")
+
